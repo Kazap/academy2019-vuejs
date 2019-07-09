233eba433f5d7971a7c82965c0becbe6ab3cab81
rogeralbinoi <roger.albino.i@gmail.com>
2019-07-09 15:41:19 -0300

    chore(vue): adiciona exemplos de componentes

---
+++ 2-vuejs/6-components/js/main.js
@@ -0,0 +1,18 @@
+Vue.component('todo-item', {
+  // O componente todo-item agora aceita uma
+  // "prop", que é como um atributo personalizado.
+  // Esta propriedade foi chamada de "todo".
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetais' },
+      { id: 1, text: 'Queijo' },
+      { id: 2, text: 'Qualquer outra coisa que humanos podem comer' }
+    ]
+  }
+})
